0549ece8b15fe3611626676c80c8c2c659663f84
Vitor Oliveira <vitor.oliveiramzx@hotmail.com>
2022-03-20 17:53:24 -0300

    navBar scss 80%

--- src/components/sideBar/sideBar.tsx
+++ src/components/sideBar/sideBar.tsx
@@ -3,92 +3,97 @@ import './sideBar.scss';
 
 function SideBar() {
     return (
-        <div className="sidebar">
-            <div className="logoContent">
-                <div className="logo">
-                    <i className="fa-brands fa-github"></i>
-                    <div className="logoName">Model</div>
+        <>
+            <div className="sidebar">
+                <div className="logoContent">
+                    <div className="logo">
+                        <i className="fa-brands fa-github"></i>
+                        <div className="logoName">Model</div>
+                    </div>
+                    <i className="fa-solid fa-bars" id="btn"></i>
                 </div>
-                <i className="fa-solid fa-bars" id="btn"></i>
-            </div>
-            <ul className="navList">
-                <li>
-                    <a href="#">
-                        <i className="fa-solid fa-magnifying-glass"></i>
-                        <input type="text" placeholder="Search" />
-                    </a>
-                    {/* <span className="toolTip">Dashboard</span> */}
-                </li>
-                <li>
-                    <a href="#">
-                        <i className="fa-brands fa-dev"></i>
-                        <span className="linksName">Dashboard</span>
-                    </a>
-                    {/* <span className="toolTip">Dashboard</span> */}
-                </li>
-                <li>
-                    <a href="#">
-                        <i className="fa-regular fa-user"></i>
-                        <span className="linksName">User</span>
-                    </a>
-                    {/* <span className="toolTip">Dashboard</span> */}
-                </li>
-                <li>
-                    <a href="#">
-                        <i className="fa-regular fa-message"></i>
-                        <span className="linksName">Messages</span>
-                    </a>
-                    {/* <span className="toolTip">Dashboard</span> */}
-                </li>
-                <li>
-                    <a href="#">
-                        <i className="fa-solid fa-chart-pie"></i>
-                        <span className="linksName">Analytics</span>
-                    </a>
-                    {/* <span className="toolTip">Dashboard</span> */}
-                </li>
-                <li>
-                    <a href="#">
-                        <i className="fa-regular fa-folder"></i>
-                        <span className="linksName">File Manager</span>
-                    </a>
-                    {/* <span className="toolTip">Dashboard</span> */}
-                </li>
-                <li>
-                    <a href="#">
-                        <i className="fa-solid fa-cart-shopping"></i>
-                        <span className="linksName">Order</span>
-                    </a>
-                    {/* <span className="toolTip">Dashboard</span> */}
-                </li>
-                <li>
-                    <a href="#">
-                        <i className="fa-regular fa-heart"></i>
-                        <span className="linksName">Saved</span>
-                    </a>
-                    {/* <span className="toolTip">Dashboard</span> */}
-                </li>
-                <li>
-                    <a href="#">
-                        <i className="fa-solid fa-gear"></i>
-                        <span className="linksName">Settings</span>
-                    </a>
-                    {/* <span className="toolTip">Dashboard</span> */}
-                </li>
-            </ul>
-            <div className="profileContent">
-                <div className="profile">
-                    <div className="profileDetails">
-                        {/* <img src="https://avatars.githubusercontent.com/u/80105563?v=4" alt="pfp" /> */}
-                        <div className="nameJob">
-                            <div className="name">Vitor Oliveira</div>
-                            <div className="job">Front-End DEV</div>
+                <ul className="navList">
+                    <li>
+                        <a href="#">
+                            <i className="fa-solid fa-magnifying-glass"></i>
+                            <input type="text" placeholder="Search" />
+                        </a>
+                        {/* <span className="toolTip">Dashboard</span> */}
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i className="fa-brands fa-dev"></i>
+                            <span className="linksName">Dashboard</span>
+                        </a>
+                        {/* <span className="toolTip">Dashboard</span> */}
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i className="fa-regular fa-user"></i>
+                            <span className="linksName">User</span>
+                        </a>
+                        {/* <span className="toolTip">Dashboard</span> */}
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i className="fa-regular fa-message"></i>
+                            <span className="linksName">Messages</span>
+                        </a>
+                        {/* <span className="toolTip">Dashboard</span> */}
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i className="fa-solid fa-chart-pie"></i>
+                            <span className="linksName">Analytics</span>
+                        </a>
+                        {/* <span className="toolTip">Dashboard</span> */}
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i className="fa-regular fa-folder"></i>
+                            <span className="linksName">File Manager</span>
+                        </a>
+                        {/* <span className="toolTip">Dashboard</span> */}
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i className="fa-solid fa-cart-shopping"></i>
+                            <span className="linksName">Order</span>
+                        </a>
+                        {/* <span className="toolTip">Dashboard</span> */}
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i className="fa-regular fa-heart"></i>
+                            <span className="linksName">Saved</span>
+                        </a>
+                        {/* <span className="toolTip">Dashboard</span> */}
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i className="fa-solid fa-gear"></i>
+                            <span className="linksName">Settings</span>
+                        </a>
+                        {/* <span className="toolTip">Dashboard</span> */}
+                    </li>
+                </ul>
+                <div className="profileContent">
+                    <div className="profile">
+                        <div className="profileDetails">
+                            <img src="https://avatars.githubusercontent.com/u/80105563?v=4" alt="pfp" />
+                            <div className="nameJob">
+                                <div className="name">Vitor Oliveira</div>
+                                <div className="job">Front-End DEV</div>
+                            </div>
+                            <i className="fa-solid fa-arrow-right-from-bracket" id="logout"></i>
                         </div>
-                        <i className="fa-solid fa-arrow-right-from-bracket" id="logout"></i>
                     </div>
                 </div>
             </div>
-        </div>
+            <div className="homeContent">
+                <div className="textArea">Home Content</div>
+            </div>
+        </>
     )
 }
 
