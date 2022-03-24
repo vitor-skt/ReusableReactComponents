e359731a31bfc10833c354bdaa98b43340aa0611
Vitor Oliveira <vitor.oliveiramzx@hotmail.com>
2022-03-23 21:09:37 -0300

    new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import NavMenu from './components/navMenu/navMenu'
 import SideBar from './components/sideBar/sideBar'
 
 function App() {
   return (
     <>
-      <SideBar />
+      <NavMenu />
+      {/* <SideBar /> */}
     </>
   )
 }
---
+++ src/components/navMenu/navMenu.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function NavMenu() {
+    return (
+        <>
+
+            <nav>
+                <div className="navContent">
+
+                    <div className="toggleButton">
+                        <i className="fa-solid fa-plus"></i>
+                    </div>
+                    <span>
+                        <a href="#"><i className="fa-solid fa-house"></i></a>
+                    </span>
+                    <span>
+                        <a href="#"><i className="fa-solid fa-camera"></i></a>
+                    </span>
+                    <span>
+                        <a href="#"><i className="fa-solid fa-stopwatch"></i></a>
+                    </span>
+                    <span>
+                        <a href="#"><i className="fa-solid fa-stopwatch"></i></a>
+                    </span>
+                </div>
+            </nav>
+
+        </>
+    )
+}
+
+export default NavMenu
